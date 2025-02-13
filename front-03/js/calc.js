ad881b824bc80efbc0742183c88427a5e84cab37
giovannirp <giovanni_ge@hotmail.com>
2025-02-13 11:20:47 -0300

    feat: add

---
+++ front-03/js/calc.js
@@ -0,0 +1,15 @@
+// Declaração de variáveis
+let num1 = prompt("Digite um número");
+let num2 = prompt("Digite outro número");
+
+// Operações matemáticas
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+// Exibindo os resultados no console
+console.log("O resultado da soma é: " + soma);
+console.log("O resultado da Sub é: " + sub);
+console.log("O resultado da mult é: " + mult);
+console.log("O resultado da divisão é: " + div);

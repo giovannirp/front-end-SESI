39a0e52a947418e664e52af88545f67261e955cd
giovannirp <giovanni_ge@hotmail.com>
2025-02-20 09:50:14 -0300

    adicionando

---
+++ front-04/funcao.js
@@ -0,0 +1,16 @@
+// Criando uma função
+function minhaFuncao() {
+    alert("Olá");
+}
+
+function dados(nome, idade) {
+    console.log(nome + idade);
+}
+
+// Chamando função
+dados("Giovanni ", 37);
+dados("Carla ", 40);
+dados("Juliana ", 8);
+dados("Doris ", 13);
+
+// minhaFuncao();

8fb5a8c049a6c7e0039efeb843211b67e9264eef
giovannirp <giovanni_ge@hotmail.com>
2025-02-20 08:43:28 -0300

    Adicionando variaveis e tipos

---
+++ front-04/variavel.js
@@ -0,0 +1,9 @@
+// Criando variáveis
+let nome = "Giovanni"; // String
+let idade = 37; // Int
+let casado = false; // Booleano
+
+// Imprimindo variáveis
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof casado);

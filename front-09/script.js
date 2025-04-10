fb57dd27907dc9d4605fff4921077133bcc891a3
giovannirp <giovanni_ge@hotmail.com>
2025-04-10 08:53:04 -0300

    feat: adicionando estrutura html, css e JavaScript

---
+++ front-09/script.js
@@ -0,0 +1,25 @@
+const cars = [
+    {
+        brand: "Toyota",
+        model: "corolla",
+        year: 2020
+    },
+    {
+        brand: "Honda",
+        model: "Civic",
+        year: 2019
+    },
+    {
+        brand: "Ford",
+        model: "Fiesta",
+        year: 2014
+    },
+    {
+        brand: "Ford",
+        model: "Focus",
+        year: 2018
+    }
+];
+
+console.log(cars[0].brand);
+console.log(cars[2].model);
